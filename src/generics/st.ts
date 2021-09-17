7bcfb8167353bfd8cd09e02c927742daf5245454
Evan Welsh <noreply@evanwelsh.com>
2021-09-16 20:01:02 -0700

    Correctly type scrollbars in St

--- src/generics/st.ts
+++ src/generics/st.ts
@@ -12,6 +12,7 @@ export default {
         const Bin = namespace.assertClass("Bin");
         const Button = namespace.assertClass("Button");
         const ScrollView = namespace.assertClass("ScrollView");
+        const ScrollBar = namespace.assertClass("ScrollBar");
         const Widget = namespace.assertClass("Widget");
         // TODO: Create a way to propagate this generic to child classes.
         const Viewport = namespace.assertClass("Viewport");
@@ -78,6 +79,22 @@ export default {
             default: Actor.getType(),
             constraint: Actor.getType()
         });
+
+        const get_hscroll_bar = ScrollView.members.find(member => member.name === 'get_hscroll_bar');
+        const get_vscroll_bar = ScrollView.members.find(member => member.name === 'get_vscroll_bar')
+        
+        if (get_hscroll_bar) {
+            const fixed_get_h = get_hscroll_bar?.copy({ returnType: ScrollBar.getType() });
+       
+            const index = ScrollView.members.indexOf(get_hscroll_bar)
+            ScrollView.members.splice(index, 1, fixed_get_h);
+        }
+
+        if (get_vscroll_bar) {
+            const fixed_get_v = get_vscroll_bar?.copy({ returnType: ScrollBar.getType() });
+            const index = ScrollView.members.indexOf(get_vscroll_bar)
+            ScrollView.members.splice(index, 1, fixed_get_v);
+        }
 
         ScrollView.addGeneric({
             deriveFrom: Bin.getType(),
