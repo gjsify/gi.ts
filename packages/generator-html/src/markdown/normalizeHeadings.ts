34aef45adc10d018a33f82db6f50fd61a512cdce
Evan Welsh <noreply@evanwelsh.com>
2022-07-06 17:57:15 -0400

    generator-html: Fix header normalizing types

--- packages/generator-html/src/markdown/normalizeHeadings.ts
+++ packages/generator-html/src/markdown/normalizeHeadings.ts
@@ -3,11 +3,14 @@ import visit from 'unist-util-visit';
 export default function () {
   return (tree: any) => {
     visit(tree, node => {
+      // @ts-expect-error Missing types for heading
       const depth = node.depth;
       if (node.type === 'heading' && typeof depth === 'number') {
         if (depth < 3) {
+          // @ts-expect-error Missing types for heading
           node.depth = 3;
         } else {
+          // @ts-expect-error Missing types for heading
           node.depth = depth;
         }
       }
