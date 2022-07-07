d40147f66985e37f44b5d813e7d702268ce4bcf7
Evan Welsh <noreply@evanwelsh.com>
2022-07-06 20:22:50 -0400

    lib: Fix bug breaking generating non-private record fields

--- src/gir/class.ts
+++ src/gir/class.ts
@@ -908,7 +908,7 @@ export class GirClass extends GirBaseClass {
       if (klass.field) {
         klass.field
           .filter(isIntrospectable)
-          .filter(field => "private" in field.$ && field.$.private !== "1")
+          .filter(field => !("private" in field.$) || field.$.private !== "1")
           .filter(field => !("callback" in field) && !field.$.name.startsWith("_"))
           .forEach(field => {
             const f = GirField.fromXML(modName, ns, options, null, field);
@@ -1221,7 +1221,7 @@ export class GirRecord extends GirBaseClass {
         clazz.fields.push(
           ...klass.field
             .filter(isIntrospectable)
-            .filter(field => "private" in field.$ && field.$.private !== "1")
+            .filter(field => !("private" in field.$) || field.$.private !== "1")
             // TODO investigate these field callbacks.
             // Record fields can just be mirrors of existing functions.
             .filter(field => !("callback" in field))
